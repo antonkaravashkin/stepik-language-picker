e6ec1a13857451c4faebbee44eee6b8b51c41372
Anton Karavashkin <url1990@ahoo.com>
2021-11-16 14:20:58 +0400

    fixed_language_options

--- test_items.py
+++ test_items.py
@@ -1,10 +1,12 @@
 from selenium.webdriver.common.by import By
 from conftest import browser
+import time
 
-link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/'
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 
 def test_user_should_see_add_bucket_button(browser):
     browser.get(link)
     browser.implicitly_wait(5)
-    browser.find_element(By.XPATH, "//button[contains(@class, 'btn-add-to-basket')]")
+    message = browser.find_element(By.XPATH, "//button[contains(@class, 'btn-add-to-basket')]")    
+    assert message, "Кнопка не найдена"
